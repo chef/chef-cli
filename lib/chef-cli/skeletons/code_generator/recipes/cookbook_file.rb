55f091515c1f3593cfd2cab0812dc7b569194c71
Pranay Singh <i5singh.pranay@gmail.com>
2021-01-04 18:04:50 +0530

    This corrects an issue where an error occurs if someone tries to generate a cookbook file in a nested directory.
    As an example, `chef generate file cookbooks/email_handler/handlers email_handler.rb` now creates a hierarchy:
    
    ```
    cookbooks/email_handler
    ├── files
    │       └── handlers
    │           └── email_handler.rb
    ```
    
    Previously it would fail with `Error executing action `create` on resource 'template[cookbooks/email_handler/files/default/handlers/email_handler.rb]'`
    
    Fixes #91 s
    
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- lib/chef-cli/skeletons/code_generator/recipes/cookbook_file.rb
+++ lib/chef-cli/skeletons/code_generator/recipes/cookbook_file.rb
@@ -1,7 +1,10 @@
 context = ChefCLI::Generator.context
 cookbook_dir = File.join(context.cookbook_root, context.cookbook_name)
-files_dir = File.join(cookbook_dir, 'files')
-cookbook_file_path = File.join(files_dir, context.new_file_basename)
+new_file_basename = File.basename(context.new_file_basename)
+relative_path = File.dirname(context.new_file_basename)
+relative_path.slice! "."
+files_dir = File.join(cookbook_dir, 'files', relative_path)
+cookbook_file_path = File.join(files_dir, new_file_basename)
 
 directory files_dir do
   recursive true
@@ -19,5 +22,4 @@ else
     source 'cookbook_file.erb'
     helpers(ChefCLI::Generator::TemplateHelper)
   end
-
 end

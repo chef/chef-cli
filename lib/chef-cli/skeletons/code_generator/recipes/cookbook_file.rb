0a174cc4c044661af7fd6b36e59393b31c1ebe1d
Pranay Singh <i5singh.pranay@gmail.com>
2021-01-04 16:48:21 +0530

    chef generate file cookbooks/email_handler handlers/email_handler.rb commands broken into correct hierarchy
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- lib/chef-cli/skeletons/code_generator/recipes/cookbook_file.rb
+++ lib/chef-cli/skeletons/code_generator/recipes/cookbook_file.rb
@@ -1,7 +1,18 @@
 context = ChefCLI::Generator.context
 cookbook_dir = File.join(context.cookbook_root, context.cookbook_name)
 files_dir = File.join(cookbook_dir, 'files')
-cookbook_file_path = File.join(files_dir, context.new_file_basename)
+path_arr = context.new_file_basename.split('/')
+if path_arr.size > 1
+  new_file_basename = path_arr.last
+  path_arr.pop
+  path_arr.each do |ele|
+    files_dir = File.join(files_dir, ele)
+  end
+  cookbook_file_path = File.join(files_dir, new_file_basename)
+else
+  cookbook_file_path = File.join(files_dir, context.new_file_basename) 
+end
+
 
 directory files_dir do
   recursive true

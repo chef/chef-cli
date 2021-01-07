d5693bce6560248dfb36d2704175f8fdf2bef7f1
Pranay Singh <i5singh.pranay@gmail.com>
2021-01-07 18:38:16 +0530

    Linting file with rubocop chefstyle
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- lib/chef-cli/skeletons/code_generator/recipes/cookbook_file.rb
+++ lib/chef-cli/skeletons/code_generator/recipes/cookbook_file.rb
@@ -4,14 +4,14 @@ files_dir = File.join(cookbook_dir, 'files')
 file_basename = context.new_file_basename
 path_arr = file_basename.split('/')
 if path_arr.size > 1
-   new_file_basename = path_arr.last
-   path_arr.pop
-   path_arr.each do |ele|
-     files_dir = File.join(files_dir, ele)
-   end
-   cookbook_file_path = File.join(files_dir, new_file_basename)
+  new_file_basename = path_arr.last
+  path_arr.pop
+  path_arr.each do |ele|
+    files_dir = File.join(files_dir, ele)
+  end
+  cookbook_file_path = File.join(files_dir, new_file_basename)
 else
-  cookbook_file_path = File.join(files_dir, context.new_file_basename) 
+  cookbook_file_path = File.join(files_dir, context.new_file_basename)
 end
 
 directory files_dir do

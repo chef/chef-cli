1fafc811aa732cb7d2c43d6875bb167e41166d7b
Pranay Singh <i5singh.pranay@gmail.com>
2022-08-22 17:36:46 +0530

    correcting linting for randomhex
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- lib/chef-cli/policyfile_services/export_repo.rb
+++ lib/chef-cli/policyfile_services/export_repo.rb
@@ -120,7 +120,7 @@ module ChefCLI
         # p = Process.pid
         # t = Time.new.utc.strftime("%Y%m%d%H%M%S") # commenting out as we need to tweak long path which cause issue while exporting in windows machine
         # path = "chefcli-export-#{p}-#{t}"
-        require 'securerandom'
+        require "securerandom" unless defined?(SecureRandom)
         random_string = SecureRandom.hex(2)
         path = "chef-export-#{random_string}"
         Dir.mktmpdir(path) do |d|

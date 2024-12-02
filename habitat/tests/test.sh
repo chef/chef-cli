409d9e74608ed92de4bf9c056420bbd1e9359a6f
nitin sanghi <nsanghi@progress.com>
2024-12-02 14:55:43 +0530

    sudo for installing hab pkg
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- habitat/tests/test.sh
+++ habitat/tests/test.sh
@@ -21,7 +21,7 @@ package_version=$(awk -F / '{print $3}' <<<"$pkg_ident")
 cd "${project_root}"
 
 echo "--- :mag_right: Testing ${pkg_ident} executables"
-actual_version=$(hab pkg exec "${pkg_ident}" chef-cli -- -v | sed -E 's/.*Version ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+actual_version=$(hab pkg exec "${pkg_ident}" chef-cli -v | sed -E 's/.*Version ([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
 [[ "$package_version" = "$actual_version" ]] || error "chef-cli is not the expected version. Expected '$package_version', got '$actual_version'"
 
 echo "--- :Running rake"

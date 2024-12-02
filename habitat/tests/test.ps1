0924d5de174d3e81da5e2c6ef470dd69eff48174
nitin sanghi <nsanghi@progress.com>
2024-12-02 19:48:48 +0530

    Change the message in test phase and throw error
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- habitat/tests/test.ps1
+++ habitat/tests/test.ps1
@@ -9,10 +9,12 @@ Write-Host "--- :fire: Smokish test"
 $version=hab pkg exec "${pkg_ident}" chef-cli -v
 $actual_version=[Regex]::Match($version,"([0-9]+.[0-9]+.[0-9]+)").Value
 $package_version=$PackageIdentifier.split("/",4)[2]
+Write-BuildLine "package_version  $package_version actual version $actual_version"
 if ($package_version -eq $actual_version)
 {
     Write "Chef-cli working fine"
 }
 else {
     Write-Error "chef-cli version not met expected $package_version actual version $actual_version "
+    throw "Chef cli windows pipeline not working for hab pkg"
 }

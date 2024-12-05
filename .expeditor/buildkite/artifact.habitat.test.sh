df8d347f42cc6accbfedbd48dfc207b8c568bc5f
nitin sanghi <nsanghi@progress.com>
2024-12-05 18:45:02 +0530

    adding locales
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- .expeditor/buildkite/artifact.habitat.test.sh
+++ .expeditor/buildkite/artifact.habitat.test.sh
@@ -54,7 +54,7 @@ if [ -f ./results/last_build.env ]; then
     . ./results/last_build.env
     export pkg_artifact
 fi
-sudo yum install locales-all
+yum install locales-all
 
 echo "+++ Installing ${pkg_ident:?is undefined}"
 echo "++++"

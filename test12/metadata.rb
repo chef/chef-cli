096cc9e66f32c75920d6fe69744efa9da4a29115
nikhil2611 <nikhilgupta2102@gmail.com>
2025-01-24 19:45:17 +0530

    nikhil/test_branch2
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- test12/metadata.rb
+++ test12/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures test12'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are

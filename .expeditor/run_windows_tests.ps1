343af733b6d5a0d3e2fa0ee8fa250b9a833366b9
Ashique Saidalavi <Ashique.saidalavi@progress.com>
2025-09-10 13:03:23 +0530

    Removed the temp code
    Signed-off-by: Ashique Saidalavi <Ashique.saidalavi@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,7 +1,4 @@
 $ErrorActionPreference="stop"
-Write-Host "Manually clearing the chef-test-kitchen-enterprise cache"
-Remove-Item -Recurse -Force "C:/workdir/vendor/bundle/ruby/3.1.0/cache/bundler/git/chef-test-kitchen-enterprise-dba8545c33365a2bffe55f9cb935af5b46709af2"
-Write-Host "Cleaned the previous cache"
 
 Write-Host "--- bundle install"
 

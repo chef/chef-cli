b7c5dcd9e101dc0169fd95017cd0642b79cfc9e8
nikhil2611 <nikhilgupta2102@gmail.com>
2025-09-10 14:30:23 +0530

    reverted back the cache fix code
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -1,13 +1,5 @@
 $ErrorActionPreference="stop"
 
-Write-Host "Manually clearing the chef-test-kitchen-enterprise cache"
-if (Test-Path "C:/workdir/vendor/bundle/ruby/3.1.0/cache/bundler/git/chef-test-kitchen-enterprise-dba8545c33365a2bffe55f9cb935af5b46709af2") {
-    Remove-Item -Recurse -Force "C:/workdir/vendor/bundle/ruby/3.1.0/cache/bundler/git/chef-test-kitchen-enterprise-dba8545c33365a2bffe55f9cb935af5b46709af2"
-    Write-Host "Cleaned the previous cache"
-} else {
-    Write-Host "Cache directory does not exist"
-}
-
 Write-Host "--- bundle install"
 
 bundle config --local path vendor/bundle

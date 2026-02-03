2107cb38186bd2381a6c22870caa125dbe8b0317
nikhil2611 <ngupta@progress.com>
2026-02-03 16:25:47 +0530

    finxing windows hab test pipeline
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- post-bundle-install.rb
+++ post-bundle-install.rb
@@ -16,15 +16,17 @@ Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
   matches = File.basename(gempath).match(/.*-[A-Fa-f0-9]{12}/)
   next unless matches
 
-  gem_name = File.basename(Dir["#{gempath}/*.gemspec"].first, ".gemspec")
-  # FIXME: should strip any valid ruby platform off of the gem_name if it matches
+  Dir["#{gempath}/*.gemspec"].each do |gemspec_path|
+    gem_name = File.basename(gemspec_path, ".gemspec")
+    # FIXME: should strip any valid ruby platform off of the gem_name if it matches
 
-  next unless gem_name
+    next unless gem_name
 
-  puts "re-installing #{gem_name}..."
+    puts "re-installing #{gem_name}..."
 
-  Dir.chdir(gempath) do
-    system("gem build #{gem_name}.gemspec") or raise "gem build failed"
-    system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
+    Dir.chdir(gempath) do
+      system("gem build #{gem_name}.gemspec") or raise "gem build failed"
+      system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
+    end
   end
 end

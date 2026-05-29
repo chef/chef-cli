4509318930f9ac4575de9c41ed658090b4912a61
nikhil2611 <ngupta@progress.com>
2026-05-29 16:59:55 +0530

    align plan.sh and binstub_patch.rb with chef's appbundler pattern
    - Simplified binstub_patch.rb to match chef/chef (only GEM_PATH + APPBUNDLER_ALLOW_RVM)
    - Updated do_setup_environment to use push_runtime_env/set_runtime_env
    - Fixed do_prepare to use proper /usr/bin/env symlink pattern with do_end cleanup
    - Reordered do_install: fix_interpreter vendor/bin -> appbundler -> sed binstub_patch
    - Reference external binstub_patch.rb file instead of inline generation
    - Removed fix_interpreter on bin/* (not needed, matches chef pattern)
    
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- binstub_patch.rb
+++ binstub_patch.rb
@@ -1,8 +1,4 @@
 unless ENV["APPBUNDLER_ALLOW_RVM"]
   ENV["APPBUNDLER_ALLOW_RVM"] = "true"
-  bin_path = __dir__
-  vendor_path = File.expand_path(File.join(bin_path, "..", "vendor"))
-  ENV["GEM_HOME"] = vendor_path
-  ENV["GEM_PATH"] = [vendor_path, ENV["GEM_PATH"]].compact.join(File::PATH_SEPARATOR)
-  ENV["PATH"] = [bin_path, File.join(vendor_path, "bin"), RbConfig::CONFIG["bindir"], ENV["PATH"]].compact.join(File::PATH_SEPARATOR)
+  ENV["GEM_PATH"] = [File.expand_path(File.join(__dir__, "..", "vendor")), ENV["GEM_PATH"]].compact.join(File::PATH_SEPARATOR)
 end
--- habitat/plan.sh
+++ habitat/plan.sh
@@ -5,35 +5,41 @@ pkg_origin=chef
 ruby_pkg="core/ruby3_4"
 pkg_deps=(${ruby_pkg} core/coreutils core/libarchive)
 pkg_build_deps=(
-    core/make
-    core/sed
-    core/gcc
-    core/git
-    )
+  core/make
+  core/sed
+  core/gcc
+  core/git
+)
 pkg_bin_dirs=(bin)
 
 do_setup_environment() {
-  build_line 'Setting GEM_HOME="$pkg_prefix/vendor"'
-  export GEM_HOME="$pkg_prefix/vendor"
+  push_runtime_env GEM_PATH "${pkg_prefix}/vendor"
 
-  build_line "Setting GEM_PATH=$GEM_HOME"
-  export GEM_PATH="$GEM_HOME"
+  set_runtime_env APPBUNDLER_ALLOW_RVM "true" # prevent appbundler from clearing out the carefully constructed runtime GEM_PATH
+  set_runtime_env LANG "en_US.UTF-8"
+  set_runtime_env LC_CTYPE "en_US.UTF-8"
 }
+
 do_prepare() {
-  ln -sf "$(pkg_interpreter_for core/ruby3_4 bin/ruby)" "$(pkg_interpreter_for core/coreutils bin/env)"
+  if [[ ! -f /usr/bin/env ]]; then
+    ln -s "$(pkg_interpreter_for core/coreutils bin/env)" /usr/bin/env
+  fi
 }
+
 pkg_version() {
   cat "$SRC_PATH/VERSION"
 }
+
 do_before() {
   update_pkg_version
 }
+
 do_unpack() {
   mkdir -pv "$HAB_CACHE_SRC_PATH/$pkg_dirname"
   cp -RT "$PLAN_CONTEXT"/.. "$HAB_CACHE_SRC_PATH/$pkg_dirname/"
 }
-do_build() {
 
+do_build() {
     export GEM_HOME="$pkg_prefix/vendor"
 
     build_line "Setting GEM_PATH=$GEM_HOME"
@@ -48,9 +54,10 @@ do_build() {
     ruby ./cleanup_gem_lockfiles.rb
     ruby ./post-bundle-install.rb
 }
+
 do_install() {
 
-  # Copy NOTICE.TXT to the package directory
+  # Copy NOTICE to the package directory
   if [[ -f "$PLAN_CONTEXT/../NOTICE" ]]; then
     build_line "Copying NOTICE to package directory"
     cp "$PLAN_CONTEXT/../NOTICE" "$pkg_prefix/"
@@ -58,34 +65,23 @@ do_install() {
     build_line "Warning: NOTICE not found at $PLAN_CONTEXT/../NOTICE"
   fi
 
-   export GEM_HOME="$pkg_prefix/vendor"
+  export GEM_HOME="$pkg_prefix/vendor"
 
   build_line "Setting GEM_PATH=$GEM_HOME"
   export GEM_PATH="$GEM_HOME"
   gem install chef-cli-*.gem --no-document
   ruby ./cleanup_lint_roller.rb
 
-  build_line "** generating binstubs for chef-cli with precise version pins"
-  "$(pkg_path_for $ruby_pkg)/bin/ruby" "${pkg_prefix}/vendor/bin/appbundler" . "$pkg_prefix/bin" chef-cli
+  build_line "** fixing binstub shebangs"
+  fix_interpreter "${pkg_prefix}/vendor/bin/*" "$ruby_pkg" bin/ruby
 
-  build_line "** generating binstub_patch.rb"
-  cat > binstub_patch.rb <<'PATCH'
-unless ENV["APPBUNDLER_ALLOW_RVM"]
-  ENV["APPBUNDLER_ALLOW_RVM"] = "true"
-  bin_path = __dir__
-  vendor_path = File.expand_path(File.join(bin_path, "..", "vendor"))
-  ENV["GEM_HOME"] = vendor_path
-  ENV["GEM_PATH"] = [vendor_path, ENV["GEM_PATH"]].compact.join(File::PATH_SEPARATOR)
-  ENV["PATH"] = [bin_path, File.join(vendor_path, "bin"), RbConfig::CONFIG["bindir"], ENV["PATH"]].compact.join(File::PATH_SEPARATOR)
-end
-PATCH
+  build_line "** generating binstubs for chef-cli with precise version pins"
+  "${pkg_prefix}/vendor/bin/appbundler" . "$pkg_prefix/bin" chef-cli
 
   build_line "** patching binstubs to allow running directly"
   for binstub in ${pkg_prefix}/bin/*; do
-    sed -i "/require \"rubygems\"/r binstub_patch.rb" "$binstub"
+    sed -i "/require \"rubygems\"/r ${PLAN_CONTEXT}/../binstub_patch.rb" "$binstub"
   done
-
-  fix_interpreter "${pkg_prefix}/bin/*" "$ruby_pkg" bin/ruby
 
   build_line "** creating wrapper for runtime environment"
   mkdir -p "$pkg_prefix/libexec"
@@ -107,6 +103,7 @@ EOF
   rm -rf $GEM_PATH/bundler
   rm -rf $GEM_PATH/doc
 }
+
 do_after() {
   build_line "Removing .github directories from vendored gems..."
   find "$pkg_prefix/vendor/gems" -type d -name ".github" \
@@ -122,4 +119,11 @@ do_after() {
 
 do_strip() {
   return 0
+}
+
+do_end() {
+  if [[ "$(readlink /usr/bin/env)" = "$(pkg_interpreter_for core/coreutils bin/env)" ]]; then
+    build_line "Removing the symlink we created for '/usr/bin/env'"
+    rm /usr/bin/env
+  fi
 }
